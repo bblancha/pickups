83147625a47687b6b2a0c244ca668141e30ddf2e
GermainZ <germanosz@gmail.com>
2015-02-17 16:41:23 +0200

    Don't expose hashes in the channel names

--- pickups/util.py
+++ pickups/util.py
@@ -6,21 +6,23 @@ import re
 
 CONV_HASH_LEN = 7
 
+hashes = {}
 
 def conversation_to_channel(conv):
     """Return channel name for hangups.Conversation."""
     # Must be 50 characters max and not contain space or comma.
-    conv_hash = hashlib.sha1(conv.id_.encode()).hexdigest()
     name = get_conv_name(conv).replace(',', '_').replace(' ', '')
-    return '#{}[{}]'.format(name[:50 - CONV_HASH_LEN - 3],
-                            conv_hash[:CONV_HASH_LEN])
+    name = "#{}".format(name[:49])
+    conv_hash = hashlib.sha1(conv.id_.encode()).hexdigest()
+    hashes[name] = conv_hash
+    return name
 
 
 def channel_to_conversation(channel, conv_list):
     """Return hangups.Conversation for channel name."""
-    conv_hash = re.match(r'^.*\[([a-f0-9]+)\]$', channel).group(1)
-    return {hashlib.sha1(conv.id_.encode()).hexdigest()[:CONV_HASH_LEN]: conv
-            for conv in conv_list.get_all()}[conv_hash]
+    conv_hash = hashes[channel]
+    return {hashlib.sha1(conv.id_.encode()).hexdigest(): conv for conv in
+            conv_list.get_all()}[conv_hash]
 
 
 def get_nick(user):
